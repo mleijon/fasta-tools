aa8f919d9dca577bf03c06e2996990e0b98bc26f
micke <mikael.leijon@gmail.com>
2018-10-06 00:57:02 +0200

    goda framsteg

--- split_fasta.py
+++ split_fasta.py
@@ -7,6 +7,8 @@ class SpadesFasta(FastaList):
     """docstring for ."""
     def __init__(self, spades_file):
         super(SpadesFasta,self).__init__(spades_file)
+        sum_of_cov = 0
+        sum_of_len = 0
         t = []
         seq_pars = dict()
         self.seq_par_list = []
@@ -15,10 +17,33 @@ class SpadesFasta(FastaList):
                 t = line.split('_')
                 seq_pars ={t[0][1:]:int(t[1]),t[2]:int(t[3]),t[4]:float(t[5])}
                 self.seq_par_list.append(seq_pars)
+        self.nr_of_contigs = len(self.seq_par_list)
+        for i in range(len(self.seq_par_list)):
+            sum_of_cov += self.seq_par_list[i]['cov']
+            sum_of_len += self.seq_par_list[i]['length']
+        self.aver_length = round(sum_of_len/self.nr_of_contigs,0)
+        self.aver_cov = round(sum_of_cov/self.nr_of_contigs,0)
+        self.is_spadesfasta = True
+        for i in range(len(self.seq_par_list)):
+            if 'NODE' not in self.seq_par_list[i]:
+                self.is_spadesfasta = False
+            elif 'length' not in self.seq_par_list[i]:
+                self.is_spadesfasta = False
+            elif 'cov' not in self.seq_par_list[i]:
+                self.is_spadesfasta = False
+
+#Writes a parameter file based on the ID-string created by Spades and som other parameters
+    def WrParToFile(self,parfile,inpname):
+            parfile.write('Parameters for: '+inpname+'\n\n')
+            parfile.write('Number of contigs: %d\n' % self.nr_of_contigs)
+            parfile.write('Average contig length: %d\n' % self.aver_length)
+            parfile.write('Average contig coverage: %d\n' % self.aver_cov)
+            parfile.write('_________________________________________\n\n')
+            print(self.seq_par_list)
 
 #Creates dictionary of blast *no hits* ids
 def rd_ids_bla(bla_file):
-    global nr_rmd
+    global nr_rmd       #nr of contigs removed
     no_hit = '***** No hits found ******'
     linelist = []
     no_hits = dict()
@@ -47,36 +72,6 @@ def write_filtered_fa(blast_in):
     fil_fa.close()
     fil_re.close()
 
-#Check if the file appears to be created by Spades
-def spades_file(fa_file):
-    parameters = fastalst.seq_par_list
-    for i in range(len(parameters)):
-        if 'NODE' not in parameters[i]:
-            return False
-        elif 'length' not in parameters[i]:
-            return False
-        elif 'cov' not in parameters[i]:
-            return False
-    fa_file.seek(0)
-    return True
-
-#Writes a parameter file based on the ID-string created by Spades and som other parameters
-def write_parfile (fa_file,fipa,inpname):
-    parameters = fastalst.seq_par_list
-    nr_of_contigs = len(parameters)
-    sum_of_cov = 0
-    sum_of_len = 0
-    fipa.write('Parameters for: '+inpname+'\n\n')
-    fipa.write('Number of contigs: %d\n' % nr_of_contigs)
-    for i in range(len(parameters)):
-        sum_of_cov += parameters[i]['cov']
-        sum_of_len += parameters[i]['length']
-    aver_length = round(sum_of_len/nr_of_contigs,0)
-    aver_cov = round(sum_of_cov/nr_of_contigs,0)
-    fipa.write('Average contig length: %d\n' % aver_length)
-    fipa.write('Average read coverage: %d\n' % aver_cov)
-    fipa.write('_________________________________________\n\n')
-
 #Main
 import linecache
 import argparse
@@ -87,11 +82,11 @@ parser.add_argument('-p',action='store_true',help='switch for parameter file out
 parser.add_argument('-f',type = str,help='fastafile')
 parser.add_argument('-b',type = str,help='blastfile')
 args = parser.parse_args()
-try:
-    finf = open(args.f)
-    fastalst = SpadesFasta(finf)
-except:
-    sys.exit('input file error')
+#try:
+finf = open(args.f)
+fastalst = SpadesFasta(finf)
+#except:
+#    sys.exit('input file error')
 try:
     binf = open(args.b)
 except:
@@ -101,15 +96,16 @@ try:
 except:
     binf.close()
     sys.exit('error writing file')
-if args.p and spades_file(finf):
+if args.p and fastalst.is_spadesfasta:
     if '.' in args.f:
         name = args.f[:args.f.find('.')]+'.par'
         fipa = open(name,'w')
     else:
         fipa = open(args.f+'.par','w')
-    write_parfile(finf,fipa,args.f)
+    fastalst.WrParToFile(fipa,args.f)
     fihi = open('hits_'+args.f)
-    write_parfile(fihi,fipa,'hits_'+args.f)
+    fastaHitslst = SpadesFasta(fihi)
+    fastaHitslst.WrParToFile(fipa,'hits_'+args.f)
     finf.seek(0)
     percent_remove = round(100*nr_rmd/len(fastalst.seq_list),0)
     fipa.write('%d sequences removed (%g%%)' % (nr_rmd,percent_remove))
