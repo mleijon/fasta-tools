d93f4c8f6b76fda416b7cdb668527cdb370c60fc
Mikael Leijon <mikael@linux-p4fr.suse>
2018-10-09 11:09:21 +0200

    genetic code tillagd i revTrans

--- revTrans.py
+++ revTrans.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 class revTrans(object):
     """docstring for revTrans."""
+
+    genCode = {"F":["TTT","TTC"],"L":["TTA","TTG", "CTT", "CTC", "CTA", "CTG"],["GCT", "GCC", "GCA", "GCG"],
+               "R":["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],"N":["AAT", "AAC"],"D":["GAT", "GAC"],
+               "C":["TGT", "TGC"],"Q":["CAA", "CAG"],"E":["GAA", "GAG"],"G":["GGU", "GGC", "GGA", "GGG"],
+               "H":["CAT", "CAC"],"I":["ATT", "ATC", "ATA"],"K":["AAA", "AAG"],"M":["ATG"],
+               "P":["CCU", "CCC", "CCA", "CCG"],"S":["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+               "T":["ACT", "ACC", "ACA", "ACG"],"W":["TGG"],"Y":["TAT", "TAC"],"V":["GTT", "GTC", "GTA", "GTG"],
+               "STOP":["TAA", "TGA", "TAG"]}
+
     def __init__(self, peptide):
         super(revTrans, self).__init__()
-    def 
+    def crGen(aaPos,peptide,seqLst)
