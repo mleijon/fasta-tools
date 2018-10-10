13c2ff3ae45bfa6b90d4d65762c105cd3d9a397c
Mikael Leijon <mikael@linux-p4fr.suse>
2018-10-10 17:36:15 +0200

    fungerar

--- revTrans.py
+++ revTrans.py
@@ -2,7 +2,7 @@
 class revTrans(object):
     """docstring for revTrans."""
 
-    genCode = {"F":["TTT","TTC"],"L":["TTA","TTG", "CTT", "CTC", "CTA", "CTG"],["GCT", "GCC", "GCA", "GCG"],
+    genCode = {"F":["TTT","TTC"],"L":["TTA","TTG", "CTT", "CTC", "CTA", "CTG"],"A":["GCT", "GCC", "GCA", "GCG"],
                "R":["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],"N":["AAT", "AAC"],"D":["GAT", "GAC"],
                "C":["TGT", "TGC"],"Q":["CAA", "CAG"],"E":["GAA", "GAG"],"G":["GGU", "GGC", "GGA", "GGG"],
                "H":["CAT", "CAC"],"I":["ATT", "ATC", "ATA"],"K":["AAA", "AAG"],"M":["ATG"],
@@ -12,8 +12,11 @@ class revTrans(object):
 
     def __init__(self, peptide):
         super(revTrans, self).__init__()
+        self.peptide = peptide
     genLst = []
-    def crGen(aaPos,peptide,seqLst):
-        for aa in peptide:
-            for codons in geneCode[aa]:
-                
+    def crGen(self):
+        for aa in self.peptide:
+            print(self.genCode[aa])
+#main
+test = revTrans('FL')
+test.crGen()
