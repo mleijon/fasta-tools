a02d6794f2a7bcf0eaeac78a33ccf35e3a342602
mleijon <36968756+mleijon@users.noreply.github.com>
2018-10-16 09:11:47 +0200

    fq2fa tillagd

--- fasta.py
+++ fasta.py
@@ -5,9 +5,16 @@ class FastaList(object):
         seq_list = []
         id_list = []
         newseq = ''
-        self.faFile = faFile
+        self.faName = faFile
+        if self.faName[-2:] == 'gz':
+            self.gz = True
+        if self.gz:
+            self.faFile = self.fq2fa()
+        else:
+            self.faFile = self.rdfi()
+
         first = True
-        for line in self.rdfi():
+        for line in self.faFile:
             if line.startswith('>'):
                 id_list.append(line[1:].strip())
                 if not first:
@@ -25,22 +32,23 @@ class FastaList(object):
 
     def rdfi(self):
         import gzip
-        if self.faFile[-2:] == 'gz':
-            faFiuz = open(self.faFile[:-3],'w')
-            with gzip.open(self.faFile,'rt') as f:
+        if self.gz:
+            faFiuz = open(self.faName[:-3],'w')
+            with gzip.open(self.faName,'rt') as f:
                 faFiuz.write(f.read())
                 print('Decompressing...',end='',flush=True)
             faFiuz.close()
-            faFiuz = open(self.faFile[:-3])
+            faFiuz = open(self.faName[:-3])
             print('Done decompressing',end='',flush=True)
             return faFiuz
         else:
-            faFi = open(self.faFile)
+            faFi = open(self.faName)
             return faFi
     def fq2fa(self):
-        outFa = open(self.faFile[:self.faFile.find('.gz')]+'fa')
+        outFa = open(self.faName[:self.faName.find('.gz')]+'fa')
         j = 0
         for line in self.rdfi():
+            print('converting...',end='',flush=True)
             if line[0] == '@' and j % 4 == 0:
                 outFa.write(line.replace('@','>',1))
             elif j % 4 == 1:
@@ -51,9 +59,11 @@ class FastaList(object):
                 pass
             else
                 print('Not a fastq-file')
-                break
+                return
                 outFa.close()
+        print('Done converting...',end='',flush=True)
         outFa.close()
+        return outFa
 
     def rev(self,seqLst):
         return seqLst[::-1]
