fa5e1703d6d1a85d8bffb4272b67e463a37ee875
Mikael Leijon <mikael@linux-p4fr.suse>
2018-10-22 12:23:17 +0200

    Fungerar

--- blFindVir.py
+++ blFindVir.py
@@ -16,6 +16,25 @@ def countlines(infile):
 
 
 # Something
+def ctrle(e_in, emin):
+    if e_in[0] == 'e':
+        e_out = float('1' + e_in)
+        if emin[0] == 'e':
+            r = float('1'+emin)/float(e_out)
+        else:
+            r = float(emin)/float(e_out)
+    elif float(e_in) == 0:
+        e_out = 0
+        r = 1
+    else:
+        e_out = float(e_in)
+        if emin[0] == 'e':
+            r = float('1' + emin)/float(e_in)
+        else:
+            r = float(emin)/float(e_in)
+    return [e_out, r]
+
+
 def blFindVir(inFileName, eCut):
     anchor = 'Sequences producing significant alignments: '
     hitLst = []
@@ -26,18 +45,12 @@ def blFindVir(inFileName, eCut):
     while j <= nrOfRows:
         if anchor in linecache.getline(inFileName, j):
             query = linecache.getline(inFileName, j-6).strip()[7:]
-            if linecache.getline(inFileName, j+2)[67:].split()[1].strip()[0]\
-               == 'e':
-                    eMin = float('1' + linecache.getline(inFileName, j+2)[67:].
-                                 split()[1].strip())
-            else:
-                eMin = float(linecache.getline(inFileName, j+2)[67:].
-                             split()[1].strip())
+            e = linecache.getline(inFileName, j+2)[67:].split()[1].strip()
+            emin = e
             j += 2
-            e = eMin
             while linecache.getline(inFileName, j)[0] != '>':
                 if (('virus' or 'Virus') in linecache.getline(inFileName, j)
-                        and eMin/e > eCut):
+                        and ctrle(e, emin)[1] > 0.1):
                     acln = len(linecache.getline(inFileName, j).split()[0]) + 1
                     lstEntry['Accession'] = linecache.getline(inFileName, j).\
                         split()[0][5:]
@@ -46,25 +59,14 @@ def blFindVir(inFileName, eCut):
                     lstEntry['Bitscore'] = int(linecache.
                                                getline(inFileName, j)[67:].
                                                split()[0].strip())
-                    if linecache.getline(inFileName, j)[67:].split()[1].\
-                       strip()[0] == 'e':
-                        lstEntry['e-value'] = float('1' + linecache.
-                                                    getline(inFileName, j)
-                                                    [67:].split()[1].strip())
-                    else:
-                        lstEntry['e-value'] = float(linecache.
-                                                    getline(inFileName, j)
-                                                    [67:].split()[1].strip())
-                    lstEntry['r-e-value'] = eMin/e
+
+                    lstEntry['e-value'] = ctrle(e, emin)[0]
+                    lstEntry['r-e-value'] = round(ctrle(e, emin)[1], 2)
+                    print(lstEntry)
                     hitLst.append(lstEntry)
-                    if linecache.getline(inFileName, j+1)[0] != '\n':
-                        if linecache.getline(inFileName, j+1)[67:].split()[1].\
-                         strip()[0] == 'e':
-                            e = float('1' + linecache.getline(inFileName, j+1)
-                                      [67:].split()[1].strip())
-                        else:
-                            e = float(linecache.getline(inFileName, j+1)
-                                      [67:].split()[1].strip())
+                    if linecache.getline(inFileName, j+1) != '\n':
+                        e = linecache.getline(inFileName, j+1)[67:].split()[1]\
+                         .strip()
                 j += 1
             blVirHit[query] = hitLst
         j += 1
@@ -72,4 +74,4 @@ def blFindVir(inFileName, eCut):
     return blVirHit
 
 
-print(blFindVir('newf.blast', 0.1)["NODE_26_length_7842_cov_3.707842"][1]["Accession"])
+blFindVir('2C.fastq.blast', 0.1)
