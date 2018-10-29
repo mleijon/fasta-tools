5fadcbb046a94b8b2f98e51ade44037fe77bb9d6
mleijon <mikael.leijon@gmail.com>
2018-10-29 21:25:16 +0100

    fixat med pylint

--- fasta.py
+++ fasta.py
@@ -1,3 +1,4 @@
+"""General purpose module to handle a fasta file object"""
 import sys
 
 
@@ -8,7 +9,7 @@ class FastaList():
         seq_list = []
         id_list = []
         newseq = ''
-        self.is_fastq = True  # File ca be gzipped or fastq as well as fasta
+        is_fastq = True  # File ca be gzipped or fastq as well as fasta
         self.name = fasta_name
         self.is_gzip = self.name[-2:].casefold() == 'gz'
         if '.fq' in self.name.casefold():
@@ -16,13 +17,13 @@ class FastaList():
         elif '.fastq' in self.name.casefold():
             self.fq_ext = '.fastq'
         else:
-            self.is_fastq = False
-        if self.is_fastq:
-            self.faFile = self.fq2fa()
+            is_fastq = False
+        if is_fastq:
+            self.fa_file = self.fq2fa()
         else:
-            self.faFile = self.rdfi()
+            self.fa_file = self.rdfi()
         first = True
-        for line in self.faFile:
+        for line in self.fa_file:
             if line.startswith('>'):
                 id_list.append(line[1:].strip())
                 if not first:
@@ -32,46 +33,49 @@ class FastaList():
                 first = False
                 newseq += line
         seq_list.append(newseq)
-        self.faFile.seek(0)
+        self.fa_file.seek(0)
         self.seq_list = seq_list
         self.id_list = id_list
         self.nr_seq = len(id_list)
 
     def rdfi(self):
+        """ Reads and return a fasta file unzipped if necessary"""
         import gzip
         if self.is_gzip:
-            faFiuz = open(self.name[:-3], 'w')
-            with gzip.open(self.name, 'rt') as f:
-                faFiuz.write(f.read())
-            faFiuz.close()
-            faFiuz = open(self.name[:-3])
-            return faFiuz
-        else:
-            faFi = open(self.name)
-            return faFi
+            fa_fiuz = open(self.name[:-3], 'w')
+            with gzip.open(self.name, 'rt') as in_fi:
+                fa_fiuz.write(in_fi.read())
+            fa_fiuz.close()
+            fa_fiuz = open(self.name[:-3])
+            return fa_fiuz
+        fa_fi = open(self.name)
+        return fa_fi
 
     def fq2fa(self):
-        outFa = open(self.name[:self.name.find(self.fq_ext)]+'.fa', 'w')
+        """ Reads and return a fastq file converted to fasta format"""
+        out_fa = open(self.name[:self.name.find(self.fq_ext)]+'.fa', 'w')
         j = 0
         for line in self.rdfi():
             if line[0] == '@' and j % 4 == 0:
-                outFa.write(line.replace('@', '>', 1))
+                out_fa.write(line.replace('@', '>', 1))
             elif j % 4 == 1:
-                outFa.write(line)
+                out_fa.write(line)
             elif line[0] == '+' and j % 4 == 2:
                 pass
             elif j % 4 == 3:
                 pass
             else:
-                outFa.close()
+                out_fa.close()
                 sys.exit('Not a fastq-file')
             j += 1
-        outFa.close()
-        outFa = open(self.name[:self.name.find(self.fq_ext)]+'.fa')
-        return outFa
+        out_fa.close()
+        out_fa = open(self.name[:self.name.find(self.fq_ext)]+'.fa')
+        return out_fa
 
-    def rev(self, seqLst):
-        return seqLst[::-1]
+    def rev(self):
+        """Yields the reverse of a nucleotide sequence"""
+        return self.seq_list[::-1]
 
-    def revComp(self, seqLst):
+    def rev_comp(self, seq_lst):
+        """Yields the reverse complement of a nucleotide sequence"""
         pass
