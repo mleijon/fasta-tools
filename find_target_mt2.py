1e56e0967b1676d8cc63fd241e7f97c7103aac9c
mleijon <mikael.leijon@gmail.com>
2018-11-18 14:07:33 +0100

    -

--- find_target_mt2.py
+++ find_target_mt2.py
@@ -9,8 +9,9 @@ from functools import reduce
 from fasta import FastaList
 
 
-def process_work(fasta_div, ALL_S):
+def process_work(fasta_div):
     """Creates AIV HA gene fragments around the CS"""
+    global ALL_S
     tmp_lst = []
     counter = 0
     found = set()
@@ -19,24 +20,22 @@ def process_work(fasta_div, ALL_S):
         ALL_S = [seq for seq in ALL_S if seq not in found]
         found.clear()
         for fasta_s in ALL_S:
-            fasta_s_id = fasta_s.split('\n')[0]
-            fasta_s_seq = fasta_s.split('\n')[1]
-            if fasta_s_seq.find(fasta_t_seq) != -1:
-                out_start = int(fasta_s_seq.find(fasta_t_seq) - (ARGS.l - len(
+            if fasta_s[1].find(fasta_t_seq) != -1:
+                out_start = int(fasta_s[1].find(fasta_t_seq) - (ARGS.l - len(
                     fasta_t_seq))/2)
                 out_end = int(out_start + ARGS.l+1)
-                if fasta_s_seq[fasta_s_seq.find(fasta_t_seq)-12:fasta_s_seq.
-                               find(fasta_t_seq)] in aivcs:
+                if fasta_s[1][fasta_s[1].find(fasta_t[1])-12:fasta_s[1].
+                              find(fasta_t[1])] in aivcs:
                     patho_lbl = '_HP'
                 else:
                     patho_lbl = '_LP'
-                out_seq = fasta_s_id + patho_lbl + '\n' + fasta_s_seq[
+                out_seq = fasta_s[0] + patho_lbl + '\n' + fasta_s[1][
                     out_start:out_end] + '\n'
                 tmp_lst.append(out_seq)
                 found.add(fasta_s)
         counter += 1
-        print('{:03} % completed'.format(int((100*counter/len(fasta_div)))),
-              end='\r', flush=True)
+        # print('{:03} % completed'.format(int((100*counter/len(fasta_div)))),
+        #       end='\r', flush=True)
     return tmp_lst
 
 
@@ -51,10 +50,11 @@ if __name__ == "__main__":
                         default=150)
     ARGS = PARSER.parse_args()
     FA_S = FastaList(ARGS.s)
+    manager = Manager()
+    ALL_S = manager.list([(fasta_s.split('\n')[0], fasta_s.split('\n')[1])
+                          for fasta_s in FA_S.seq_list])
     FA_T = FastaList(ARGS.t)
     fa_t_div = FA_T.divide(ARGS.p)
-    manager = Manager()
-    ALL_S = manager.list(FA_S.seq_list)
     nr_s_seq = len(ALL_S)
     nr_t_seq = len(FA_T.seq_list)
     print('searching {} subsequences in {} sequences'.format(
@@ -73,7 +73,8 @@ if __name__ == "__main__":
             json.dump(aivcs, cs_file, indent=4)
     FA_OUT = open('sources.fa', 'w')
     with Pool(processes=ARGS.p) as p:
-        re_lst = reduce(lambda x, y: x + y, p.starmap(process_work, argument))
+        re_lst = reduce(lambda x, y: x + y, p.map(process_work, fa_t_div))
+    p.close()
     print('\n\n\n{:5} sequence(s) found'.format(len(re_lst)))
     print('{:5} sequence(s) not found'.format(
      len(FA_S.seq_list) - len(re_lst)))
