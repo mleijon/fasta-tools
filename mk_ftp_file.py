0c81637c26acc35d713549a75b515e000838857b
Mikael Leijon <mikael@linux-p4fr.suse>
2018-11-27 16:42:12 +0100

    +

---
+++ mk_ftp_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import argparse
+
+PARSER = argparse.ArgumentParser(description='output filename')
+PARSER.add_argument('-f', type=str, help='output filename', required=True)
+PARSER.add_argument('-c', type=int, help='nr of files', required=True)
+ARGS = PARSER.parse_args()
+startstring = "ftp://ftp.ncbi.nlm.nih.gov/genbank/gbvrlX.seq.gz\n"
+with open(ARGS.f, 'w') as fi:
+    for i in range(1, ARGS.c + 1):
+        fi.write(startstring.replace('X', str(i)))
+fi.close()
