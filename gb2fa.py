b459f91c45581e0697f3fd30df126277d8cfb591
mleijon <mikael.leijon@gmail.com>
2018-11-29 00:32:42 +0100

    +

--- gb2fa.py
+++ gb2fa.py
@@ -2,16 +2,26 @@
 
 import argparse
 import sys
+import gzip
 from genbank import GbParse
 
+def rdfi(in_fi):
+    """ Reads and return a genbank file unzipped if necessary"""
+    if in_fi.upper().endswith('GZ'):
+        out_fi = open(in_fi[:-3],'w')
+        with gzip.open(in_fi, 'rt') as fi:
+            out_fi.write(fi.read())
+        out_fi = open(in_fi[:-3])
+    else:
+        out_fi = open(in_fi)
+    return out_fi
 
 PARSER = argparse.ArgumentParser(description='converts genbank file to fasta\
                                  file')
-
 PARSER.add_argument('-gb', type=str, help='genbank file', required=True)
 ARGS = PARSER.parse_args()
 try:
-    GB_IN = open(ARGS.gb)
+    GB_IN = rdfi(ARGS.gb)
 except IOError:
     sys.exit('input file error')
 file_list = [record + '//' for record in GB_IN.read().split('//')][:-1]
