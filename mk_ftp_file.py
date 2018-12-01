a32086e37bfd4d6a6a45837bdbdfb3b8521dfd94
mleijon <36968756+mleijon@users.noreply.github.com>
2018-12-01 16:49:58 +0100

    +

--- mk_ftp_file.py
+++ mk_ftp_file.py
@@ -2,6 +2,9 @@
 
 import argparse
 
+gb_div = ['bct', 'con', 'env', 'est', 'gss', 'htc', 'htg', 'inv', 'mam', 'pat',
+          'phg', 'pln', 'pri', 'rod', 'rod', 'set', 'sts', 'syn', 'tsa', 'una',
+          'vrl', 'vrt']
 PARSER = argparse.ArgumentParser(description='output filename')
 PARSER.add_argument('-f', type=str, help='output filename', required=True)
 PARSER.add_argument('-c', type=int, help='nr of files', required=True)
