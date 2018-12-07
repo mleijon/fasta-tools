73ec56263b6d1efd35bd3e5207a7180440f8a46d
Mikael Leijon <mikael@linux-p4fr.suse>
2018-12-07 13:45:44 +0100

    +

--- desc2spec.py
+++ desc2spec.py
@@ -5,14 +5,34 @@ import argparse
 import mmap
 
 PARSER = argparse.ArgumentParser(description='TBD')
-#PARSER.add_argument('-b', type=str, help='Fasta summary table', required=True)
+PARSER.add_argument('-b', type=str, help='Fasta summary table', required=True)
 PARSER.add_argument('-n', type=str, help='taxid name file', required=True)
 PARSER.add_argument('-a', type=str, help='acc. to taxid file', required=True)
 ARGS = PARSER.parse_args()
 
-with open(ARGS.n, 'r+b') as fb:
-    mb = mmap.mmap(fb.fileno(), 0)
-    with open(ARGS.a, 'r+b') as fa:
-        ma = mmap.mmap(fa.fileno(), 0)
-        ma.seek(ma.find(b'X52702.1'))
-        print((ma.readline()).decode('UTF-8').strip().split('\t'))
+fn = open(ARGS.n, 'r+b')
+mn = mmap.mmap(fn.fileno(), 0)
+fa = open(ARGS.a, 'r+b')
+ma = mmap.mmap(fa.fileno(), 0)
+in_file = open(ARGS.b)
+out_file = open(ARGS.b.replace('top', 'species'), 'w')
+out_file.write('accession\tspecies\tnr-of-reads\n')
+in_file.readline()
+in_file.readline()
+for line in in_file:
+    acc = line.split('|')[0].strip()
+    nr_of_reads = line.split('|')[3].strip()
+    out_file.write(acc + '\t')
+    acc = acc.split('.')[0].encode('UTF-8')
+    ma.seek(ma.find(acc))
+    taxid = ma.readline().decode('UTF-8').strip().split('\t')[2]
+    mn.seek(mn.find(taxid.encode('UTF-8')))
+    species = mn.readline().decode('UTF-8').strip().split('|')[1]
+    out_file.write(species + '\t' + nr_of_reads + '\n')
+    print(species + '\t' + nr_of_reads + '\n')
+    ma.seek(0)
+    mn.seek(0)
+fn.close()
+fa.close()
+in_file.close()
+out_file.close()
