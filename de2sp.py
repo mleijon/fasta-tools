fdb730b85d97343309183071f06918e75d55aebe
mleijon <36968756+mleijon@users.noreply.github.com>
2018-12-12 13:14:44 +0100

    +

--- de2sp.py
+++ de2sp.py
@@ -25,20 +25,21 @@ in_file.readline()
 sum_of_reads = 0
 for line in in_file:
     acc = line.split('|')[0].strip()
-    nr_of_reads = line.split('|')[3].strip()
+    nor = line.split('|')[3].strip()
     agent = {acc: nr_of_reads}
     accessions[acc]['agents'] = []
     out_file.write(acc)
 # Don't consider the version of accession.
     if ma.find(acc.split('.')[0].encode('UTF-8')) == -1:  # Acc. not found.
-        microorg['not_found']['acc'].append(acc)
-        microorg['not_found']['nor'].append(nr_of_reads)
+        microorg['not_found'].update(['acc'].append(acc), ['nor'].append(nor))
         else
     else:
         ma.seek(ma.find(acc.split('.')[0].encode('UTF-8')))
         taxid = ma.readline().decode('UTF-8').strip().split('\t')[2]
         mn.seek(mn.find(taxid.encode('UTF-8')))
         species = mn.readline().decode('UTF-8').strip().split('|')[1]
+        microorg[species]['acc'].append(acc)
+        microorg[species]['nor'].append(nr_of_reads)
     ma.seek(0)
     mn.seek(0)
 fn.close()
