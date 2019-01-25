e4febfce9f2379558a4eef745b17591fd0c13725
mleijon <36968756+mleijon@users.noreply.github.com>
2019-01-25 17:07:49 +0100

    refactoring

--- fasta.py
+++ fasta.py
@@ -4,11 +4,11 @@ fasta and unzip gzipped files"""
 import sys
 
 
-class FastaList():
-    """From a fasta text file Creates a list with the id:s of the fasta
-    sequences (id_list) and a list of the sequences with the newlines removed
-    (seq_list), suitable for searching. The sequenes list elements are strings
-    with a newline separating the id and the nucleotides sequence."""
+class FastaList:
+    """From a fasta text file (fastq or fasta) Creates a list with the id:s of
+    the fasta sequences (id_list) and a list of the sequences with the newlines
+    removed (seq_list), suitable for searching. The sequenes list elements are
+    strings with a newline separating the id and the nucleotides sequence."""
 
     def __init__(self, fasta_name):  # Initialized by a filename string
         self.seq_list = []
@@ -19,12 +19,12 @@ class FastaList():
         if (self.is_gzip and self.name[:-3].endswith(('.fq', '.FQ', '.fastq',
                                                       '.FASTQ')) or self.name.
                 endswith(('.fq', '.FQ', '.fastq', '.FASTQ'))):
-            self.fa_file = self.fq2fa()
+            self.fa_file = self.fq2fa()  # Handle file as fastq
         else:
-            self.fa_file = self.rdfi()
+            self.fa_file = self.rdfi()  # Handle file as fasta
         for line in self.fa_file:
             if line.startswith('>'):
-                if self.id_list != []:
+                if self.id_list:
                     self.seq_list.append(newseq + '\n')
                 self.id_list.append(line[1:].strip())
                 newseq = line
@@ -35,7 +35,7 @@ class FastaList():
         self.nr_seq = len(self.id_list)
 
     def rdfi(self):
-        """ Reads and return a fasta file unzipped if necessary"""
+        """ Reads and return a fasta file object unzipped if necessary"""
         import gzip
         if self.is_gzip:
             fa_fiuz = open(self.name[:-3], 'w')
@@ -49,30 +49,29 @@ class FastaList():
 
     def fq2fa(self):
         """ Reads and return a fastq file converted to fasta format"""
-        outfa_name = self.name.rpartition('/')[2][:self.name.rpartition('/')
-                                                  [2].find('.')] + '.fa'
+        # Handles input with full path
+        basename = self.name.rpartition('/')[2]
+        outfa_name = basename[:basename.find('.')] + '.fa'
         outfa_name = self.name.rpartition('/')[0] + '/' + outfa_name
         out_fa = open(outfa_name, 'w')
-        j = 0
-        for line in self.rdfi():
-            if line[0] == '@' and j % 4 == 0:
-                out_fa.write(line.replace('@', '>', 1))
-            elif j % 4 == 1:
-                out_fa.write(line)
-            elif line[0] == '+' and j % 4 == 2:
+        for line in enumerate(self.rdfi()):
+            if line[1][0] == '@' and line[0] % 4 == 0:
+                out_fa.write(line[1].replace('@', '>', 1))  # seqid line
+            elif line[0] % 4 == 1:
+                out_fa.write(line[1])  # sequence line
+            elif line[1][0] == '+' and line[0] % 4 == 2:
                 pass
-            elif j % 4 == 3:
+            elif line[0] % 4 == 3:
                 pass
             else:
                 out_fa.close()
                 sys.exit('Not a fastq-file')
-            j += 1
         out_fa.close()
         out_fa = open(outfa_name)
         return out_fa
 
     def rev(self):
-        """Yields the reverse of a nucleotide sequence"""
+        """Returns the reverse of a nucleotide sequence"""
         return self.seq_list[::-1]
 
     def rev_comp(self):
@@ -80,7 +79,7 @@ class FastaList():
         the list seq_list"""
         seq_list_rc = []
 
-        def comp(nucl):
+        def comp(nt):
             return {
                 'A': 'T',
                 'T': 'A',
@@ -97,7 +96,7 @@ class FastaList():
                 'V': 'B',
                 'D': 'H',
                 'H': 'D'
-            }[nucl.upper()]
+            }[nt.upper()]
         for fasta in self.seq_list:
             fasta_id = fasta.split('\n')[0]
             fasta_seq = fasta.split('\n')[1]
