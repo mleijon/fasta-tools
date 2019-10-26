0ae32b0d0f17fdd9e84391fb1ae669a07cba33c3
mleijon <36968756+mleijon@users.noreply.github.com>
2019-10-27 00:28:23 +0200

    working with demultiplexing

--- ampdemult.py
+++ ampdemult.py
@@ -4,16 +4,29 @@
 from fasta import FastaList
 
 
+def sep(opsys):
+    if opsys == 'nt':
+        return '\\'
+    else:
+        return '/'
+
+
 def reduce_fa(fl):
     """removes duplicate reads and adds '_nr_of _reads to the id of the
     reduced FastaList"""
     import copy
-    reddic = dict()
     reddic_new = dict()
     dnaseqs = []
     for item in fl.seq_list:
         if len(item) >= ARGS.m:
-            dnaseqs.append(item.split('\n')[1])
+            tlst = tag_fa.seq_list
+            tlst_rc = tag_fa.seq_list_revc()
+            for tagitem in range(0, tag_fa.nr_seq, 2):
+                testt = [tlst[tagitem + 1].split('\n')[1],
+                         tlst_rc[tagitem].split('\n')[1]]
+                if all(x in item for x in testt):
+                    dnaseqs.append(item.split('\n')[1])
+                    break
     for item in dnaseqs:
         reddic = copy.deepcopy(reddic_new)
         if len(reddic) == 0:
@@ -40,9 +53,10 @@ def reduce_fa(fl):
         if item[1] < ARGS.f:
             break
         count += 1
-        fl.id_list.append('{:08d}_count:{}'.format(count, item[1]))
-        fl.seq_list.append(('{}\n{}\n'.format(fl.id_list[count-1], item[0])))
-        fl.nr_seq = len(fl.seq_list)
+        fl.id_list.append('{}_{:08d}_count:{}_length:{}'.format(fl.name[fl.name.rfind(
+            'Cp'):fl.name.rfind('.fa')], count, item[1], len(item[0])))
+        fl.seq_list.append(('{}\n{}\n'.format(fl.id_list[count - 1], item[0])))
+    fl.nr_seq = len(fl.seq_list)
     return fl
 
 
@@ -52,6 +66,7 @@ if __name__ == "__main__":
     import datetime
     import getpass
     import os
+    import sys
 
     PARSER = argparse.ArgumentParser(description='Demultiplex amplicon sequen-'
                                                  'cing NGS-data (-s) based on '
@@ -59,8 +74,9 @@ if __name__ == "__main__":
                                                  'fasta-file (-t'
                                                  'Assumes the usual file '
                                                  'extensions')
-    PARSER.add_argument('-sd', type=str, help='Directory containing NGS-data'
-                                              'fasta/q files',
+    PARSER.add_argument('-id', type=str, help='Directory for input data',
+                        required=True)
+    PARSER.add_argument('-od', type=str, help='Directory for output data',
                         required=True)
     PARSER.add_argument('-t', type=str, help='tag sequence-list fasta filename',
                         required=True)
@@ -69,34 +85,45 @@ if __name__ == "__main__":
     PARSER.add_argument('-f', type=int, help='minimum nr of seqs', default=0,
                         required=False)
     ARGS = PARSER.parse_args()
+    if not os.path.isfile(ARGS.t):
+        sys.exit('No PCR tag file. Exits.')
+    if not ((ARGS.id.endswith(sep(os.name)) and
+             ARGS.od.endswith(sep(os.name)))):
+        sys.exit('Invalid directory name. Exits.')
+    if os.path.isfile(ARGS.od[:-1]):
+        print('{} is a file'.format(ARGS.od[:-1]))
+        sys.exit('Exits')
+    if os.path.isdir(ARGS.od):
+        shutil.rmtree(ARGS.od)
+    os.mkdir(ARGS.od)
     tag_fa = FastaList(ARGS.t)
-    logfile = open('demultiplex.log', 'w')
-
-    logfile.write('Log for ampli_deplx.py at {}\nUser: {}\n'.format(
+    logfile = open(ARGS.od + 'demultiplex.log', 'w')
+    logfile.write('Log for ampdemult.py at {}\nUser: {}\n'.format(
         str(datetime.datetime.now()).split('.')[0], getpass.getuser()))
     logfile.write('Minimum sequence length = {}\n'.format(ARGS.m))
-    for seqfile in os.listdir(ARGS.sd):
-        seq_fa = reduce_fa(FastaList(ARGS.sd + seqfile))
-        # if os.path.isdir(ARGS.sd + seqfile.split('.')[0]):
-        #     shutil.rmtree(ARGS.sd + seqfile.split('.')[0])
-        os.mkdir(ARGS.sd + seqfile.split('.')[0])
-        os.chdir(ARGS.sd + seqfile.split('.')[0])
+    logfile.write('Minimum nr of sequences = {}\n'.format(ARGS.f))
+    nr_of_files = len([name for name in os.listdir(ARGS.id) if
+                       os.path.isfile(ARGS.id + name)])
+    file_nr = 1
+    for seqfile in os.listdir(ARGS.id):
+        print('processing file {}/{}'.format(file_nr, nr_of_files))
+        seq_fa = reduce_fa(FastaList(ARGS.id + seqfile))
         nr_seq_demult = 0
         taglist = tag_fa.seq_list
         taglist_rc = tag_fa.seq_list_revc()
         for seq in range(seq_fa.nr_seq):
-            rc = True
-            for tag in range(tag_fa.nr_seq):
-                if not rc:
-                    test_tag = taglist[tag].split('\n')[1]
-                else:
-                    test_tag = taglist_rc[tag].split('\n')[1]
-                rc = not rc
-                if test_tag in seq_fa.seq_list[seq].split('\n')[1]:
-                    with open(tag_fa.id_list[tag] + '.fa', 'a') as fi:
+            for tag in range(0, tag_fa.nr_seq, 2):
+                test_tags = [taglist[tag+1].split('\n')[1],
+                             taglist_rc[tag].split('\n')[1]]
+                if all(x in seq_fa.seq_list[seq] for x in test_tags):
+                    with open(ARGS.od + tag_fa.id_list[tag][:-3] + '.fa', 'a')\
+                            as fi:
                         if len(seq_fa.seq_list[seq]) >= ARGS.m:
                             fi.write('>' + seq_fa.seq_list[seq])
                             nr_seq_demult += 1
+        if seq_fa.nr_seq == 0:
+            seq_fa.nr_seq = 1
         logfile.write(("{}: {} % of {} sequences demultiplexed\n".format(
-            os.path.basename(ARGS.sd + seqfile),
-            round(100*nr_seq_demult/seq_fa.nr_seq), seq_fa.nr_seq)))
+            os.path.basename(ARGS.id + seqfile),
+            round(100 * nr_seq_demult / seq_fa.nr_seq), seq_fa.nr_seq)))
+        file_nr += 1
