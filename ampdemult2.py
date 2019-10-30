8c040c4adefb9a3062556d4abd3b0905a93f6849
mleijon <mikael.leijon@gmail.com>
2019-10-30 22:30:15 +0100

    first version

--- ampdemult2.py
+++ ampdemult2.py
@@ -36,9 +36,18 @@ if __name__ == "__main__":
     nr_of_files = len([name for name in os.listdir(ARGS.id) if
                        os.path.isfile(ARGS.id + name)])
     file_nr = 1
+    sample = dict()
+    sample_reduced = dict()
     for seqfile in os.listdir(ARGS.id):
         print('\rprocessing file {}/{}'.format(file_nr, nr_of_files), end=" ")
-        inp_seq = FastaList(ARGS.id + seqfile)
-        print(inp_seq.seq_list[0])
-        exit()
+        inp_seqs = FastaList(ARGS.id + seqfile)
+        for seq in inp_seqs.seq_list:
+            seq_name = seq.split('_count:')[0][1:]
+            seq_count = int(seq.split('_count:')[1].split('_')[0])
+            seq_length = int(seq.split(':')[2].split('_')[0])
+            dna_seq = seq.split('\n')[1]
+            sample_name = seq_name.rsplit('_', 1)[0]
+            sample[seq_name] = (sample_name, dna_seq, seq_count, seq_length)
+        file_nr += 1
+
 
