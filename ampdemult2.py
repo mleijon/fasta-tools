9f29f6b3f087e9710626c6633d51589bf7874918
mleijon <mikael.leijon@gmail.com>
2019-10-31 12:21:32 +0100

    first version

--- ampdemult2.py
+++ ampdemult2.py
@@ -33,22 +33,16 @@ if __name__ == "__main__":
     if os.path.isdir(ARGS.od):
         shutil.rmtree(ARGS.od)
     os.mkdir(ARGS.od)
-    nr_of_files = len([name for name in os.listdir(ARGS.id) if
-                       os.path.isfile(ARGS.id + name)])
-    file_nr = 1
-    sample = dict()
-    sample_reduced = dict(dict())
     for seqfile in os.listdir(ARGS.id):
-        #print('\rprocessing file {}/{}'.format(file_nr, nr_of_files), end=" ")
+        sample = dict()
+        sample_reduced = dict(dict())
         inp_seqs = FastaList(ARGS.id + seqfile)
         for seq in inp_seqs.seq_list:
             seq_name = seq.split('_count:')[0][1:]
             seq_count = int(seq.split('_count:')[1].split('_')[0])
-            seq_length = int(seq.split(':')[2].split('_')[0])
             dna_seq = seq.split('\n')[1]
             sample_name = seq_name.rsplit('_', 1)[0]
-            sample[seq_name] = [sample_name, dna_seq, seq_count, seq_length]
-        file_nr += 1
+            sample[seq_name] = [sample_name, dna_seq, seq_count]
         for key1 in sample:
             if sample[key1][0] in sample_reduced.keys():
                 for key2 in sample_reduced[sample[key1][0]]:
@@ -62,7 +56,16 @@ if __name__ == "__main__":
                         break
             else:
                 sample_reduced[sample[key1][0]] = {key1: sample[key1]}
-
+        with open(ARGS.od + seqfile, 'w') as fi:
+            for key1 in sample_reduced:
+                for key2 in sample_reduced[key1]:
+                    n = key2.split('_')
+                    name = n[0] + '_' + n[1].zfill(2) + '_' + n[2].\
+                        zfill(2) + '_' + n[3].zfill(2) + '_' + n[4]
+                    dna_seq = sample_reduced[key1][key2][1]
+                    seq_count = sample_reduced[key1][key2][2]
+                    fi.write('>{}_count:{}_length:{}\n{}\n'.
+                             format(name, seq_count, len(dna_seq), dna_seq))
 
 
 
