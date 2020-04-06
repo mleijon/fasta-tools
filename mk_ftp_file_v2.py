e43abcc9062475bb840321e4f667de85432b4306
micke <mikael.leijon@gmail.com>
2020-04-06 16:25:04 +0200

    test

---
+++ mk_ftp_file_v2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import argparse
+
+base = ['bct', 'con', 'env', 'est', 'gss', 'htc', 'htg', 'inv', 'mam', 'pat',
+          'phg', 'pln', 'pri', 'rod', 'rod', 'set', 'sts', 'syn', 'tsa', 'una',
+          'vrl', 'vrt']
+PARSER = argparse.ArgumentParser(description='output filename')
+PARSER.add_argument('-f', type=str, help='output filename', required=True)
+PARSER.add_argument('-c', type=int, help='nr of files', required=True)
+ARGS = PARSER.parse_args()
+startstring = "ftp://ftp.ncbi.nlm.nih.gov/ncbi-asn1/gbvrlX.aso.gz\n"
+with open(ARGS.f, 'w') as fi:
+    for i in range(1, ARGS.c + 1):
+        fi.write(startstring.replace('X', str(i).zfill(0)))
+fi.close()
