0af177d6bc46419fa10d57c3304e02d509367b56
mleijon <36968756+mleijon@users.noreply.github.com>
2020-08-31 11:22:49 +0200

    working with demultiplexing

--- daa2species-bk.py
+++ daa2species-bk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from collections import Counter
+from collections import Counter, defaultdict
 import argparse
 
 ROOT = '1'
@@ -16,6 +16,7 @@ tax_classes = [ARCHAEA, BACTERIA, EUCARYOTA, VIRUSES,
 taxsets = {CELLULAR_ORGANISM: set(), ARCHAEA: set(), BACTERIA: set(),
            EUCARYOTA: set(), VIRUSES: set(), UNCLASSIFIED: set(),
            OTHERSEQ: set()}
+taxcounts = defaultdict(int)
 
 # THese files are needed and downloaded from ncbi:
 # https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz
@@ -89,33 +90,91 @@ def create_taxsets(fname):
     return
 
 
-def count_txclass(fname, taxclass):
-    if taxclass == ARCHAEA:
-        taxclass_set = taxsets[ARCHAEA]
-    elif taxclass == BACTERIA:
-        taxclass_set = taxsets[BACTERIA]
-    elif taxclass == EUCARYOTA:
-        taxclass_set = taxsets[EUCARYOTA]
-    elif taxclass == VIRUSES:
-        taxclass_set = taxsets[VIRUSES]
-    elif taxclass == UNCLASSIFIED:
-        taxclass_set = taxsets[UNCLASSIFIED]
-    elif taxclass == OTHERSEQ:
-        taxclass_set = taxsets[OTHERSEQ]
-    else:
-        taxclass_set = 'NONE'
-        print('No such taxonomy class. Exits.')
-        exit()
-    count = 0
+def count_txclass(fname):
     with open(fname) as f:
         for row in f:
             taxid = row.split()[1].strip()
-            if taxid in taxclass_set:
-                count += 1
+            if taxid in taxsets[ARCHAEA]:
+                taxcounts[ARCHAEA] += 1
             elif (taxid in mergedTaxids.keys()) and (mergedTaxids[taxid]
-                                                     in taxclass_set):
-                count += 1
-    return count
+                                                     in taxsets[ARCHAEA]):
+                taxcounts[ARCHAEA] += 1
+            elif taxid in taxsets[BACTERIA]:
+                taxcounts[BACTERIA] += 1
+            elif (taxid in mergedTaxids.keys()) and (mergedTaxids[taxid]
+                                                     in taxsets[BACTERIA]):
+                taxcounts[BACTERIA] += 1
+            elif taxid in taxsets[EUCARYOTA]:
+                taxcounts[EUCARYOTA] += 1
+            elif (taxid in mergedTaxids.keys()) and (mergedTaxids[taxid]
+                                                     in taxsets[EUCARYOTA]):
+                taxcounts[EUCARYOTA] += 1
+            elif taxid in taxsets[VIRUSES]:
+                taxcounts[VIRUSES] += 1
+            elif (taxid in mergedTaxids.keys()) and (mergedTaxids[taxid]
+                                                     in taxsets[VIRUSES]):
+                taxcounts[VIRUSES] += 1
+            elif taxid in taxsets[UNCLASSIFIED]:
+                taxcounts[UNCLASSIFIED] += 1
+            elif (taxid in mergedTaxids.keys()) and (mergedTaxids[taxid]
+                                                     in taxsets[UNCLASSIFIED]):
+                taxcounts[UNCLASSIFIED] += 1
+            elif taxid in taxsets[OTHERSEQ]:
+                taxcounts[OTHERSEQ] += 1
+            elif (taxid in mergedTaxids.keys()) and (mergedTaxids[taxid]
+                                                     in taxsets[OTHERSEQ]):
+                taxcounts[OTHERSEQ] += 1
+    return
+
+
+def parse_files(m, d, n):
+    # Creates dictionary with merged taxids
+    with open(m) as f:
+        for line in f:
+            item = line.split('|')
+            mergedTaxids.update({item[0].strip(): item[1].strip()})
+
+    # Creates a set with deleted taxids
+    with open(d) as f:
+        for line in f:
+            item = line.split('|')[0].strip()
+            deletedTaxids.add(item)
+
+    # Creates a dictionary with key:values equal taxid:scientific name
+    with open(n) as f:
+        for line in f:
+            item = line.split('|')
+            if "scientific" in item[3]:
+                sciNames.update({item[0].strip(): item[1].strip()})
+
+
+def write_summary (fname):
+    with open(fname, 'w') as f:
+        f.write('Total nr of reads: {:>31}\n'.format(nr_of_reads))
+        f.write('\nNo hits to db: {:>35} ({} %)\n'.format(nr_of_nohits[0], (
+            round(100 * (nr_of_nohits[0] / nr_of_reads), 2))))
+        f.write('Could not be classified by db: {:>19} ({} %)\n'.format(
+            nr_of_nohits[1], (round(100 * (nr_of_nohits[1] / nr_of_reads), 2))))
+        f.write('Taxid deleted from db: {:>27} ({} %)\n'.format(
+            deleted_count, (round(100 * (deleted_count / nr_of_reads), 2))))
+        f.write('\nClassified by db: {:>32} ({} %)\n'.format(
+            SUM_IN_DB, (round(100 * (SUM_IN_DB / nr_of_reads), 2))))
+        f.write('------------------------------------------------------------\n')
+        f.write('CELLULAR ORGANISM: {:>31} ({} %)\n'.format(cellorg_count, (
+            round(100 * (cellorg_count / nr_of_reads), 2))))
+        f.write('ARCHAEA: {:>41} ({} %)\n'.format(taxcounts[ARCHAEA], (
+            round(100 * (taxcounts[ARCHAEA] / nr_of_reads), 2))))
+        f.write('BACTERIA: {:>40} ({} %)\n'.format(taxcounts[BACTERIA], (
+            round(100 * (taxcounts[BACTERIA] / nr_of_reads), 2))))
+        f.write('EUCARYOTA: {:>39} ({} %)\n'.format(taxcounts[EUCARYOTA], (
+            round(100 * (taxcounts[EUCARYOTA] / nr_of_reads), 2))))
+        f.write('VIRUS: {:>43} ({} %)\n'.format(taxcounts[VIRUSES], (
+            round(100 * (taxcounts[VIRUSES] / nr_of_reads), 2))))
+        f.write('UNCLASSIFIED: {:>36} ({} %)\n'.format(
+            taxcounts[UNCLASSIFIED], (round(100 * (
+                    taxcounts[UNCLASSIFIED] / nr_of_reads), 2))))
+        f.write('OTHERSEQ: {:>40} ({} %)\n'.format(taxcounts[OTHERSEQ], (
+            round(100 * (taxcounts[OTHERSEQ] / nr_of_reads), 2))))
 
 
 if __name__ == "__main__":
@@ -134,25 +193,8 @@ if __name__ == "__main__":
     nr_of_reads = count_rows(ARGS.f)
     nr_of_nohits = count_0and1(ARGS.f)
     create_taxsets("taxidlineage.dmp")
-
-    # Creates dictionary with merged taxids
-    with open(merged_file) as f:
-        for line in f:
-            item = line.split('|')
-            mergedTaxids.update({item[0].strip(): item[1].strip()})
-
-    # Creates a set with deleted taxids
-    with open(deleted_file) as f:
-        for line in f:
-            item = line.split('|')[0].strip()
-            deletedTaxids.add(item)
-
-    # Creates a dictionary with key:values equal taxid:scientific name
-    with open(names_file) as f:
-        for line in f:
-            item = line.split('|')
-            if "scientific" in item[3]:
-                sciNames.update({item[0].strip(): item[1].strip()})
+    parse_files(merged_file, deleted_file, names_file)
+    count_txclass(ARGS.f)
 
     with open(ARGS.f) as f:
         deleted_count = 0
@@ -171,37 +213,8 @@ if __name__ == "__main__":
                     Results += [sciNames[item]]
 
     Results = Counter(Results).most_common()
-    SUM_IN_DB = 0
-    SUM_IN_DB += count_txclass(ARGS.f, ARCHAEA)
-    SUM_IN_DB += count_txclass(ARGS.f, BACTERIA)
-    SUM_IN_DB += count_txclass(ARGS.f, EUCARYOTA)
-    SUM_IN_DB += count_txclass(ARGS.f, VIRUSES)
-    SUM_IN_DB += count_txclass(ARGS.f, UNCLASSIFIED)
-    SUM_IN_DB += count_txclass(ARGS.f, OTHERSEQ)
-    SUM_IN_DB += cellorg_count
 
-    print('Total nr of reads: {:>31}'.format(nr_of_reads))
-    print('\nNo hits to db: {:>35} ({} %)'.format(nr_of_nohits[0], (
-        round(100 * (nr_of_nohits[0] / nr_of_reads), 2))))
-    print('Could not be classified by db: {:>19} ({} %)'.format(
-        nr_of_nohits[1], (round(100 * (nr_of_nohits[1] / nr_of_reads), 2))))
-    print('Taxid deleted from db: {:>27} ({} %)'.format(
-        deleted_count, (round(100 * (deleted_count / nr_of_reads), 2))))
-    print('\nClassified by db: {:>32} ({} %)'.format(
-        SUM_IN_DB, (round(100 * (SUM_IN_DB / nr_of_reads), 2))))
-    print('------------------------------------------------------------')
-    print('CELLULAR ORGANISM: {:>31} ({} %)'.format(cellorg_count, (
-        round(100 * (cellorg_count / nr_of_reads), 2))))
-    print('ARCHAEA: {:>41} ({} %)'.format(count_txclass(ARGS.f, ARCHAEA), (
-        round(100 * (count_txclass(ARGS.f, ARCHAEA) / nr_of_reads), 2))))
-    print('BACTERIA: {:>40} ({} %)'.format(count_txclass(ARGS.f, BACTERIA), (
-        round(100 * (count_txclass(ARGS.f, BACTERIA) / nr_of_reads), 2))))
-    print('EUCARYOTA: {:>39} ({} %)'.format(count_txclass(ARGS.f, EUCARYOTA), (
-        round(100 * (count_txclass(ARGS.f, EUCARYOTA) / nr_of_reads), 2))))
-    print('VIRUS: {:>43} ({} %)'.format(count_txclass(ARGS.f, VIRUSES), (
-        round(100 * (count_txclass(ARGS.f, VIRUSES) / nr_of_reads), 2))))
-    print('UNCLASSIFIED: {:>36} ({} %)'.format(
-        count_txclass(ARGS.f, UNCLASSIFIED), (round(100 * (
-                count_txclass(ARGS.f, UNCLASSIFIED) / nr_of_reads), 2))))
-    print('OTHERSEQ: {:>40} ({} %)'.format(count_txclass(ARGS.f, OTHERSEQ), (
-        round(100 * (count_txclass(ARGS.f, OTHERSEQ) / nr_of_reads), 2))))
+    SUM_IN_DB = sum(taxcounts.values()) + cellorg_count
+    if ARGS.s:
+        summary_file_name = ARGS.f.split('.')[0] + '_summary.txt'
+        write_summary(summary_file_name)
