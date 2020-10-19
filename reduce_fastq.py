6ee6ded11992c9291b8b0f51aa674b7abc60b4bd
micke <mikael.leijon@gmail.com>
2020-10-19 18:06:07 +0200

    Fungerande version!

---
+++ reduce_fastq.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+def count_char(fname, ch):
+    with open(fname) as f:
+        return f.read().count(ch)
+
+
+def reduce_fastq(fname_in, fact):
+    import linecache
+    import random
+    nr_of_reads = count_char(fname_in, '@')
+    sample_size = round(nr_of_reads/fact)
+    read_lst = list(range(nr_of_reads))
+    selected_reads = random.sample(read_lst, sample_size)
+    r1_new = fname_in.rpartition('/')[0] + fname_in.rpartition('/')[1] + 'r_' + fname_in.rpartition('/')[2]
+    r2_new = r1_new.replace('_R1_', '_R2_')
+    with open(r1_new, 'w') as r1_out, open(r2_new, 'w') as r2_out:
+        for read in selected_reads:
+            for i in range(4):
+                r1_out.write(linecache.getline(fname_in, read + i))
+                r2_out.write(linecache.getline(fname_in.replace('_R1_', '_R2_'), read + i))
+
+
+if __name__ == "__main__":
+    import argparse
+    PARSER = argparse.ArgumentParser(description='TBD')
+    PARSER.add_argument('-f', type=str, help='input R1 fastq file', required=True)
+    ARGS = PARSER.parse_args()
+    reduce_fastq(ARGS.f, 100)
