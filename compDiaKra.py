dc7dd4a0e28855afde8ad953338797640e39ef5e
micke <mikael.leijon@gmail.com>
2020-10-20 16:40:14 +0200

    Fungerande version!

--- compDiaKra.py
+++ compDiaKra.py
@@ -12,23 +12,10 @@ def count_char(fname, ch):
         return f.read().count(ch)
 
 
-def reduce_fastq(fname_in, fname_out, fact):
-    import linecache
-    import random
-    nr_of_reads = count_char(fname_in, '@')
-    sample_size = round(nr_of_reads/fact)
-    read_lst = list(range(nr_of_reads))
-    selected_reads = random.sample(read_lst, sample_size)
-    with open(fname_out, 'w') as f_out:
-        for read in selected_reads:
-            for i in range(4):
-                f_out.write(linecache.getline(fname_in, read + i))
-
-
 if __name__ == "__main__":
     import argparse
     PARSER = argparse.ArgumentParser(description='TBD')
-    PARSER.add_argument('-fq', type=str, help='input kraken file',
+    PARSER.add_argument('-fq', type=str, help='input fastq file',
                         required=True)
     PARSER.add_argument('-ik', type=str, help='input kraken file',
                         required=False)
@@ -37,4 +24,5 @@ if __name__ == "__main__":
     PARSER.add_argument('-o', type=str, help='output kraken file',
                         required=False)
     ARGS = PARSER.parse_args()
-    reduce_fastq(ARGS.fq, 'test.fastq', 100)
+    print(count_char(ARGS.fq, '@'))
+
--- reduce_fastq.py
+++ reduce_fastq.py
@@ -10,16 +10,20 @@ def reduce_fastq(fname_in, fact):
     import linecache
     import random
     nr_of_reads = count_char(fname_in, '@')
+    print('{} sequence reads'.format(nr_of_reads))
     sample_size = round(nr_of_reads/fact)
-    read_lst = list(range(nr_of_reads))
+    read_lst = list(range(1, nr_of_reads, 4))
     selected_reads = random.sample(read_lst, sample_size)
     r1_new = fname_in.rpartition('/')[0] + fname_in.rpartition('/')[1] + 'r_' + fname_in.rpartition('/')[2]
     r2_new = r1_new.replace('_R1_', '_R2_')
     with open(r1_new, 'w') as r1_out, open(r2_new, 'w') as r2_out:
         for read in selected_reads:
+            print('reads: {}'.format(read))
             for i in range(4):
                 r1_out.write(linecache.getline(fname_in, read + i))
+                linecache.clearcache()
                 r2_out.write(linecache.getline(fname_in.replace('_R1_', '_R2_'), read + i))
+                linecache.clearcache()
 
 
 if __name__ == "__main__":
@@ -27,4 +31,4 @@ if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description='TBD')
     PARSER.add_argument('-f', type=str, help='input R1 fastq file', required=True)
     ARGS = PARSER.parse_args()
-    reduce_fastq(ARGS.f, 100)
+    reduce_fastq(ARGS.f, 1000)
