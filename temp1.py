edf0c901245e7d82f8197f6f586d04bea3ae2f40
mleijon <mikael.leijon@gmail.com>
2021-04-28 12:04:45 +0200

    temp1

---
+++ temp1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import argparse
+taxonomy = dict()
+tax_lvls = ['k', 'd', 'p', 'c', 'o', 'f', 'g', 's']
+PARSER = argparse.ArgumentParser(description='TBD')
+PARSER.add_argument('-f', type=str, help='input fasta file', required=True)
+PARSER.add_argument('-t', type=str, help='input taxonomy file', required=True)
+#PARSER.add_argument('-o', type=str, help='output file', required=True)
+ARGS = PARSER.parse_args()
+with open(ARGS.t) as f_t, open(ARGS.f) as f_f:
+    taxdata = f_t.readlines()
+    seqdata = f_f.read().split('>')[1:]
+for item in taxdata:
+    item_acc = item.split('\t')[0]
+    item_tax = item.split('\t')[1]
+    item_tax = item_tax.replace('__', ':')
+    item_tax = item_tax.replace(';', ',')
+    item_tax = item_tax.replace(' ', '')
+    item_tax = item_tax.replace('\n', ';\n')
+    for level in tax_lvls:
+        if level + ':,' in item_tax:
+            item_tax = item_tax.replace(level + ':,', '')
+    item_tax = 'tax=' + item_tax
+    taxonomy[item_acc] = item_tax
+
+print(taxonomy['AGTN01422275.5.1489'])
+print(taxonomy['AGRF01000040.13758.15228'])
+
+
+
